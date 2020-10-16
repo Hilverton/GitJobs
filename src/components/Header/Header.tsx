bd984b0ad68b918b42767f539cab6760c0c8e38b
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-10-16 10:55:44 -0300

    Criação do componente Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+export default function Header() {
+  return (
+    <header className='header'>
+      <nav>
+        <h1>GitJobs</h1>
+      </nav>
+    </header>
+  );
+}
