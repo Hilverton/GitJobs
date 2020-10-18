dd7062df7924dd536b48f2f38834caf682716b8a
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-10-17 21:30:44 -0300

    Atualizações e criação da seção de formulário
    1. Conexão ao fontawesome
    2. Criação do componente Input
    3. Seção do formulário na página Home, mobile-first

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import Header from './Header';
 import Card from './Card';
+import Input from './Input';
 
-export { Header, Card };
+export { Header, Card, Input };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import { Header, Card } from '../../components';
+import { Header, Card, Input } from '../../components';
 
 import './styles.css';
 
@@ -19,13 +19,41 @@ export default function Home() {
       }
     }
 
-    // apiData();
+    apiData();
   }, []);
 
   return (
     <div className='home'>
       <Header />
-      <main className='main'>
+
+      <section className='home__form'>
+        <form>
+          <div className='home__form__element'>
+            <Input
+              icon='fas fa-search'
+              placeholder='Filter by title, companies, expertise...'
+            />
+          </div>
+          <div className='home__form__element'>
+            <Input
+              icon='fas fa-map-marker-alt'
+              placeholder='Filter by location...'
+            />
+          </div>
+
+          <div className='home__form__element home__form__element--last'>
+            <div className='home__form__element__wrapper__check'>
+              <input type='checkbox' />
+              <label className='home__form__element__label' htmlFor=''>
+                Full time only
+              </label>
+            </div>
+            <button className='home__form__element__button'>Search</button>
+          </div>
+        </form>
+      </section>
+
+      <main className='home__main'>
         {jobs.map((job) => (
           <Card key={job.id} data={job} />
         ))}
