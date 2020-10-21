a2853279f7acd8291a315b61c564606033592535
Emusc <44450158+Hilverton@users.noreply.github.com>
2020-10-21 15:35:05 -0300

    Correção na página Detail e no componente Shimmer
    1. Se der erro na requisição a api na página Detail é feito redirecionamento para a página de erro
    2. Adição do componente Shimmer na página Detail para o caso de conexão lenta com a internet ter um feedback visual

--- src/pages/Detail/Detail.tsx
+++ src/pages/Detail/Detail.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { StaticContext } from 'react-router';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, useHistory } from 'react-router-dom';
 
+import { Shimmer } from '../../components';
 import { GetTime } from '../../utils';
 import noImage from '../../assets/no_image.png';
 
@@ -11,22 +12,30 @@ export default function Detail({
   match,
   location,
 }: RouteComponentProps<DetailsParams, StaticContext, DetailsLocation>) {
+  const history = useHistory();
   const [job, setJob] = useState<JobsType>();
+  const [load, setLoad] = useState(false);
 
   useEffect(() => {
+    window.scrollTo(0, 0);
+
+    setLoad(true);
+
     async function getData() {
       const response = await fetch(
         `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions/${match.params.id}.json`,
       );
       if (response.ok) {
         const data = await response.json();
-        console.log(data);
         setJob(data);
+      } else {
+        history.push('/*');
       }
     }
-    if (location.state.job) setJob(location.state.job);
+    if (location.state) setJob(location.state.job);
     else getData();
-  }, [match.params.id, location.state.job]);
+    setLoad(false);
+  }, [match.params.id, location.state, history]);
 
   function cleanUrl(url: string) {
     return url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, '').split('/')[0];
@@ -44,80 +53,99 @@ export default function Detail({
 
   return (
     <div className='detail'>
-      <header className='detail__header'>
-        <img
-          className='detail__header__img'
-          src={job?.company_logo || noImage}
-          alt='Companie Logo'
-        />
-        <div className='detail__header__info'>
-          <div>
-            <h1 className='detail__header__info__title'>{job?.company}</h1>
-            <p className='detail__header__info__company'>
-              {cleanUrl(job?.company_url || '')}
-            </p>
-          </div>
-          <a
-            className='detail__header__info__btn'
-            href={job?.company_url}
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Company Site
-          </a>
-        </div>
-      </header>
+      {load ? (
+        <>
+          <header className='detail__load__header'>
+            <Shimmer />
+          </header>
+          <main className='detail__load__main'>
+            <Shimmer />
+          </main>
+          <section className='detail__load__how__apply'>
+            <Shimmer />
+          </section>
+          <footer className='detail__load__footer'>
+            <Shimmer />
+          </footer>
+        </>
+      ) : (
+        <>
+          <header className='detail__header'>
+            <img
+              className='detail__header__img'
+              src={job?.company_logo || noImage}
+              alt='Companie Logo'
+            />
+            <div className='detail__header__info'>
+              <div>
+                <h1 className='detail__header__info__title'>{job?.company}</h1>
+                <p className='detail__header__info__company'>
+                  {cleanUrl(job?.company_url || '')}
+                </p>
+              </div>
+              <a
+                className='detail__header__info__btn'
+                href={job?.company_url}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                Company Site
+              </a>
+            </div>
+          </header>
+          <main className='detail__main'>
+            <header className='detail__main__header'>
+              <div>
+                <span className='detail__main__info'>
+                  {GetTime(job?.created_at || '')} • {job?.type}
+                </span>
 
-      <main className='detail__main'>
-        <header className='detail__main__header'>
-          <div>
-            <span className='detail__main__info'>
-              {GetTime(job?.created_at || '')} • {job?.type}
-            </span>
+                <h3 className='detail__main__job'>{job?.title}</h3>
 
-            <h3 className='detail__main__job'>{job?.title}</h3>
+                <p className='detail__main__location'>{job?.location}</p>
+              </div>
+              <a
+                href={getApplyLink(job?.how_to_apply || '')}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='detail__apply__btn'
+              >
+                Apply Now
+              </a>
+            </header>
+            <section
+              className='detail__main__description'
+              dangerouslySetInnerHTML={html(job?.description || '')}
+            ></section>
+          </main>
+          <section className='detail__how__apply'>
+            <h1>How to Apply</h1>
+            <div
+              className='detail__how__apply__link'
+              dangerouslySetInnerHTML={html(job?.how_to_apply || '')}
+            ></div>
+          </section>
 
-            <p className='detail__main__location'>{job?.location}</p>
-          </div>
-          <a
-            href={getApplyLink(job?.how_to_apply || '')}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='detail__apply__btn'
-          >
-            Apply Now
-          </a>
-        </header>
-        <section
-          className='detail__main__description'
-          dangerouslySetInnerHTML={html(job?.description || '')}
-        ></section>
-      </main>
-
-      <section className='detail__how__apply'>
-        <h1>How to Apply</h1>
-        <div
-          className='detail__how__apply__link'
-          dangerouslySetInnerHTML={html(job?.how_to_apply || '')}
-        ></div>
-      </section>
-
-      <footer className='detail__footer'>
-        <div className='detaill__footer__content'>
-          <div>
-            <h1 className='detaill__footer__content__job'>{job?.title}</h1>
-            <p className='detaill__footer__content__company'>{job?.company}</p>
-          </div>
-          <a
-            href={getApplyLink(job?.how_to_apply || '')}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='detail__apply__btn'
-          >
-            Apply Now
-          </a>
-        </div>
-      </footer>
+          <footer className='detail__footer'>
+            <div className='detaill__footer__content'>
+              <div>
+                <h1 className='detaill__footer__content__job'>{job?.title}</h1>
+                <p className='detaill__footer__content__company'>
+                  {job?.company}
+                </p>
+              </div>
+              <a
+                href={getApplyLink(job?.how_to_apply || '')}
+                target='_blank'
+                rel='noopener noreferrer'
+                className='detail__apply__btn'
+              >
+                Apply Now
+              </a>
+            </div>
+          </footer>
+        </>
+      )}
     </div>
   );
 }
